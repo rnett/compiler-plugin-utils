070f8982f2dced684ec2327d30b0c7bf5752e206
Ryan Nett <JNett96@gmail.com>
2021-05-26 18:29:02 -0700

    Deep copy unspecified replacements

--- compiler-plugin-utils/src/main/kotlin/com/rnett/plugin/ir/SymbolReplacer.kt
+++ compiler-plugin-utils/src/main/kotlin/com/rnett/plugin/ir/SymbolReplacer.kt
@@ -6,9 +6,7 @@ import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.symbols.IrClassifierSymbol
 import org.jetbrains.kotlin.ir.symbols.IrConstructorSymbol
 import org.jetbrains.kotlin.ir.symbols.IrEnumEntrySymbol
-import org.jetbrains.kotlin.ir.symbols.IrExternalPackageFragmentSymbol
 import org.jetbrains.kotlin.ir.symbols.IrFieldSymbol
-import org.jetbrains.kotlin.ir.symbols.IrFileSymbol
 import org.jetbrains.kotlin.ir.symbols.IrFunctionSymbol
 import org.jetbrains.kotlin.ir.symbols.IrLocalDelegatedPropertySymbol
 import org.jetbrains.kotlin.ir.symbols.IrPropertySymbol
@@ -17,8 +15,6 @@ import org.jetbrains.kotlin.ir.symbols.IrScriptSymbol
 import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
 import org.jetbrains.kotlin.ir.symbols.IrSymbol
 import org.jetbrains.kotlin.ir.symbols.IrTypeAliasSymbol
-import org.jetbrains.kotlin.ir.symbols.IrTypeParameterSymbol
-import org.jetbrains.kotlin.ir.symbols.IrValueParameterSymbol
 import org.jetbrains.kotlin.ir.symbols.IrValueSymbol
 import org.jetbrains.kotlin.ir.symbols.IrVariableSymbol
 import org.jetbrains.kotlin.ir.util.DeepCopyIrTreeWithSymbols
@@ -27,24 +23,73 @@ import org.jetbrains.kotlin.ir.util.deepCopyWithSymbols
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
+@PublishedApi
+internal class ReferenceSymbolRemapper(private val referenceRemapper: ReferenceRemapper, private val base: SymbolRemapper) : SymbolRemapper by base {
+    override fun getReferencedClass(symbol: IrClassSymbol): IrClassSymbol =
+        referenceRemapper.getReferencedClass(symbol) ?: base.getReferencedClass(symbol)
+
+    override fun getReferencedScript(symbol: IrScriptSymbol): IrScriptSymbol =
+        referenceRemapper.getReferencedScript(symbol) ?: base.getReferencedScript(symbol)
+
+    override fun getReferencedClassOrNull(symbol: IrClassSymbol?): IrClassSymbol? =
+        referenceRemapper.getReferencedClassOrNull(symbol) ?: base.getReferencedClassOrNull(symbol)
+
+    override fun getReferencedEnumEntry(symbol: IrEnumEntrySymbol): IrEnumEntrySymbol =
+        referenceRemapper.getReferencedEnumEntry(symbol) ?: base.getReferencedEnumEntry(symbol)
+
+    override fun getReferencedVariable(symbol: IrVariableSymbol): IrVariableSymbol =
+        referenceRemapper.getReferencedVariable(symbol) ?: base.getReferencedVariable(symbol)
+
+    override fun getReferencedLocalDelegatedProperty(symbol: IrLocalDelegatedPropertySymbol): IrLocalDelegatedPropertySymbol =
+        referenceRemapper.getReferencedLocalDelegatedProperty(symbol) ?: base.getReferencedLocalDelegatedProperty(symbol)
+
+    override fun getReferencedField(symbol: IrFieldSymbol): IrFieldSymbol =
+        referenceRemapper.getReferencedField(symbol) ?: base.getReferencedField(symbol)
+
+    override fun getReferencedConstructor(symbol: IrConstructorSymbol): IrConstructorSymbol =
+        referenceRemapper.getReferencedConstructor(symbol) ?: base.getReferencedConstructor(symbol)
+
+    override fun getReferencedValue(symbol: IrValueSymbol): IrValueSymbol =
+        referenceRemapper.getReferencedValue(symbol) ?: base.getReferencedValue(symbol)
+
+    override fun getReferencedFunction(symbol: IrFunctionSymbol): IrFunctionSymbol =
+        referenceRemapper.getReferencedFunction(symbol) ?: base.getReferencedFunction(symbol)
+
+    override fun getReferencedProperty(symbol: IrPropertySymbol): IrPropertySymbol =
+        referenceRemapper.getReferencedProperty(symbol) ?: base.getReferencedProperty(symbol)
+
+    override fun getReferencedSimpleFunction(symbol: IrSimpleFunctionSymbol): IrSimpleFunctionSymbol =
+        referenceRemapper.getReferencedSimpleFunction(symbol) ?: base.getReferencedSimpleFunction(symbol)
+
+    override fun getReferencedReturnableBlock(symbol: IrReturnableBlockSymbol): IrReturnableBlockSymbol =
+        referenceRemapper.getReferencedReturnableBlock(symbol) ?: base.getReferencedReturnableBlock(symbol)
+
+    override fun getReferencedClassifier(symbol: IrClassifierSymbol): IrClassifierSymbol =
+        referenceRemapper.getReferencedClassifier(symbol) ?: base.getReferencedClassifier(symbol)
+
+    override fun getReferencedTypeAlias(symbol: IrTypeAliasSymbol): IrTypeAliasSymbol =
+        referenceRemapper.getReferencedTypeAlias(symbol) ?: base.getReferencedTypeAlias(symbol)
+}
+
 /**
- * A symbol remapper that only replaces references to symbols
+ * A symbol remapper that only replaces references to symbols.  Returning `null` will deep copy the symbol.
  */
-public open class ReferenceRemapper : SymbolRemapper.Empty() {
-    final override fun getDeclaredClass(symbol: IrClassSymbol): IrClassSymbol = symbol
-    final override fun getDeclaredScript(symbol: IrScriptSymbol): IrScriptSymbol = symbol
-    final override fun getDeclaredFunction(symbol: IrSimpleFunctionSymbol): IrSimpleFunctionSymbol = symbol
-    final override fun getDeclaredProperty(symbol: IrPropertySymbol): IrPropertySymbol = symbol
-    final override fun getDeclaredField(symbol: IrFieldSymbol): IrFieldSymbol = symbol
-    final override fun getDeclaredFile(symbol: IrFileSymbol): IrFileSymbol = symbol
-    final override fun getDeclaredConstructor(symbol: IrConstructorSymbol): IrConstructorSymbol = symbol
-    final override fun getDeclaredEnumEntry(symbol: IrEnumEntrySymbol): IrEnumEntrySymbol = symbol
-    final override fun getDeclaredExternalPackageFragment(symbol: IrExternalPackageFragmentSymbol): IrExternalPackageFragmentSymbol = symbol
-    final override fun getDeclaredVariable(symbol: IrVariableSymbol): IrVariableSymbol = symbol
-    final override fun getDeclaredLocalDelegatedProperty(symbol: IrLocalDelegatedPropertySymbol): IrLocalDelegatedPropertySymbol = symbol
-    final override fun getDeclaredTypeParameter(symbol: IrTypeParameterSymbol): IrTypeParameterSymbol = symbol
-    final override fun getDeclaredValueParameter(symbol: IrValueParameterSymbol): IrValueParameterSymbol = symbol
-    final override fun getDeclaredTypeAlias(symbol: IrTypeAliasSymbol): IrTypeAliasSymbol = symbol
+public interface ReferenceRemapper {
+    public fun getReferencedClass(symbol: IrClassSymbol): IrClassSymbol? = null
+    public fun getReferencedScript(symbol: IrScriptSymbol): IrScriptSymbol? = null
+    public fun getReferencedClassOrNull(symbol: IrClassSymbol?): IrClassSymbol? = null
+    public fun getReferencedEnumEntry(symbol: IrEnumEntrySymbol): IrEnumEntrySymbol? = null
+    public fun getReferencedVariable(symbol: IrVariableSymbol): IrVariableSymbol? = null
+    public fun getReferencedLocalDelegatedProperty(symbol: IrLocalDelegatedPropertySymbol): IrLocalDelegatedPropertySymbol? = null
+    public fun getReferencedField(symbol: IrFieldSymbol): IrFieldSymbol? = null
+    public fun getReferencedConstructor(symbol: IrConstructorSymbol): IrConstructorSymbol? = null
+    public fun getReferencedValue(symbol: IrValueSymbol): IrValueSymbol? = null
+    public fun getReferencedFunction(symbol: IrFunctionSymbol): IrFunctionSymbol? = null
+    public fun getReferencedProperty(symbol: IrPropertySymbol): IrPropertySymbol? = null
+    public fun getReferencedSimpleFunction(symbol: IrSimpleFunctionSymbol): IrSimpleFunctionSymbol? = null
+    public fun getReferencedReturnableBlock(symbol: IrReturnableBlockSymbol): IrReturnableBlockSymbol? = null
+    public fun getReferencedClassifier(symbol: IrClassifierSymbol): IrClassifierSymbol? = null
+    public fun getReferencedTypeAlias(symbol: IrTypeAliasSymbol): IrTypeAliasSymbol? = null
 }
 
 /**
@@ -53,15 +98,15 @@ public open class ReferenceRemapper : SymbolRemapper.Empty() {
 public inline fun <reified T : IrElement> T.deepCopyAndRemapReferences(
     initialParent: IrDeclarationParent? = null,
     referenceRemapper: ReferenceRemapper
-): T = deepCopyWithSymbols(initialParent) { _, typeRemapper ->
-    DeepCopyIrTreeWithSymbols(referenceRemapper, typeRemapper)
+): T = deepCopyWithSymbols(initialParent) { symbolRemapper, typeRemapper ->
+    DeepCopyIrTreeWithSymbols(ReferenceSymbolRemapper(referenceRemapper, symbolRemapper), typeRemapper)
 }
 
 /**
- * Remap value symbols.  If given a [IrVariableSymbol], must return a [IrVariableSymbol].
+ * Remap value symbols.  If given a [IrVariableSymbol], must return a [IrVariableSymbol].  Returning `null` will deep copy the symbol.
  */
-public fun interface ValueRemapper {
-    public fun remapValue(symbol: IrValueSymbol): IrValueSymbol
+public fun interface ValueRemapper : ReferenceRemapper {
+    public fun remapValue(symbol: IrValueSymbol): IrValueSymbol?
 }
 
 /**
@@ -70,29 +115,29 @@ public fun interface ValueRemapper {
 public inline fun <reified T : IrElement> T.deepCopyAndRemapValues(
     initialParent: IrDeclarationParent? = null,
     referenceRemapper: ValueRemapper
-): T = deepCopyAndRemapReferences(initialParent, object : ReferenceRemapper() {
-    override fun getReferencedValue(symbol: IrValueSymbol): IrValueSymbol = referenceRemapper.remapValue(symbol)
-    override fun getReferencedVariable(symbol: IrVariableSymbol): IrVariableSymbol = referenceRemapper.remapValue(symbol) as IrVariableSymbol
+): T = deepCopyAndRemapReferences(initialParent, object : ReferenceRemapper {
+    override fun getReferencedValue(symbol: IrValueSymbol): IrValueSymbol? = referenceRemapper.remapValue(symbol)
+    override fun getReferencedVariable(symbol: IrVariableSymbol): IrVariableSymbol? = referenceRemapper.remapValue(symbol) as IrVariableSymbol?
 })
 
 /**
- * Remap references to functions
+ * Remap references to functions.  Returning `null` will deep copy the symbol.
  */
 public fun interface FunctionRemapper {
-    public fun remapValue(symbol: IrSimpleFunctionSymbol): IrSimpleFunctionSymbol
+    public fun remapValue(symbol: IrSimpleFunctionSymbol): IrSimpleFunctionSymbol?
 }
 
 /**
- * Deep copy and replace references to values
+ * Deep copy and replace references to values.
  */
 public inline fun <reified T : IrElement> T.deepCopyAndRemapFunctions(
     initialParent: IrDeclarationParent? = null,
     referenceRemapper: FunctionRemapper
-): T = deepCopyAndRemapReferences(initialParent, object : ReferenceRemapper() {
-    override fun getReferencedFunction(symbol: IrFunctionSymbol): IrFunctionSymbol =
+): T = deepCopyAndRemapReferences(initialParent, object : ReferenceRemapper {
+    override fun getReferencedFunction(symbol: IrFunctionSymbol): IrFunctionSymbol? =
         if (symbol is IrSimpleFunctionSymbol) referenceRemapper.remapValue(symbol) else symbol
 
-    override fun getReferencedSimpleFunction(symbol: IrSimpleFunctionSymbol): IrSimpleFunctionSymbol =
+    override fun getReferencedSimpleFunction(symbol: IrSimpleFunctionSymbol): IrSimpleFunctionSymbol? =
         referenceRemapper.remapValue(symbol)
 })
 
@@ -109,9 +154,9 @@ public inline fun ReferenceReplacements(builder: ReferenceReplacements.Builder.(
 /**
  * A list of symbol replacements, providing a [ReferenceRemapper].
  */
-public class ReferenceReplacements private constructor(private val map: Map<IrSymbol, IrSymbol>) : ReferenceRemapper() {
+public class ReferenceReplacements private constructor(private val map: Map<IrSymbol, IrSymbol>) : ReferenceRemapper {
     public operator fun contains(symbol: IrSymbol): Boolean = symbol in map
-    public operator fun <T : IrSymbol> get(symbol: T): T = (map[symbol] ?: symbol) as T
+    public operator fun <T : IrSymbol> get(symbol: T): T? = map[symbol] as T?
 
     public class Builder @PublishedApi internal constructor() {
         private val map = mutableMapOf<IrSymbol, IrSymbol>()
@@ -123,21 +168,21 @@ public class ReferenceReplacements private constructor(private val map: Map<IrSy
         internal fun build(): ReferenceReplacements = ReferenceReplacements(map.toMap())
     }
 
-    override fun getReferencedClass(symbol: IrClassSymbol): IrClassSymbol = this[symbol]
-    override fun getReferencedScript(symbol: IrScriptSymbol): IrScriptSymbol = this[symbol]
-    override fun getReferencedClassOrNull(symbol: IrClassSymbol?): IrClassSymbol? = symbol?.let { map[symbol] as IrClassSymbol? }
-    override fun getReferencedEnumEntry(symbol: IrEnumEntrySymbol): IrEnumEntrySymbol = this[symbol]
-    override fun getReferencedVariable(symbol: IrVariableSymbol): IrVariableSymbol = this[symbol]
-    override fun getReferencedLocalDelegatedProperty(symbol: IrLocalDelegatedPropertySymbol): IrLocalDelegatedPropertySymbol = this[symbol]
-    override fun getReferencedField(symbol: IrFieldSymbol): IrFieldSymbol = this[symbol]
-    override fun getReferencedConstructor(symbol: IrConstructorSymbol): IrConstructorSymbol = this[symbol]
-    override fun getReferencedValue(symbol: IrValueSymbol): IrValueSymbol = this[symbol]
-    override fun getReferencedFunction(symbol: IrFunctionSymbol): IrFunctionSymbol = this[symbol]
-    override fun getReferencedProperty(symbol: IrPropertySymbol): IrPropertySymbol = this[symbol]
-    override fun getReferencedSimpleFunction(symbol: IrSimpleFunctionSymbol): IrSimpleFunctionSymbol = this[symbol]
-    override fun getReferencedReturnableBlock(symbol: IrReturnableBlockSymbol): IrReturnableBlockSymbol = this[symbol]
-    override fun getReferencedClassifier(symbol: IrClassifierSymbol): IrClassifierSymbol = this[symbol]
-    override fun getReferencedTypeAlias(symbol: IrTypeAliasSymbol): IrTypeAliasSymbol = this[symbol]
+    override fun getReferencedClass(symbol: IrClassSymbol): IrClassSymbol? = this[symbol]
+    override fun getReferencedScript(symbol: IrScriptSymbol): IrScriptSymbol? = this[symbol]
+    override fun getReferencedClassOrNull(symbol: IrClassSymbol?): IrClassSymbol? = symbol?.let { this[symbol] }
+    override fun getReferencedEnumEntry(symbol: IrEnumEntrySymbol): IrEnumEntrySymbol? = this[symbol]
+    override fun getReferencedVariable(symbol: IrVariableSymbol): IrVariableSymbol? = this[symbol]
+    override fun getReferencedLocalDelegatedProperty(symbol: IrLocalDelegatedPropertySymbol): IrLocalDelegatedPropertySymbol? = this[symbol]
+    override fun getReferencedField(symbol: IrFieldSymbol): IrFieldSymbol? = this[symbol]
+    override fun getReferencedConstructor(symbol: IrConstructorSymbol): IrConstructorSymbol? = this[symbol]
+    override fun getReferencedValue(symbol: IrValueSymbol): IrValueSymbol? = this[symbol]
+    override fun getReferencedFunction(symbol: IrFunctionSymbol): IrFunctionSymbol? = this[symbol]
+    override fun getReferencedProperty(symbol: IrPropertySymbol): IrPropertySymbol? = this[symbol]
+    override fun getReferencedSimpleFunction(symbol: IrSimpleFunctionSymbol): IrSimpleFunctionSymbol? = this[symbol]
+    override fun getReferencedReturnableBlock(symbol: IrReturnableBlockSymbol): IrReturnableBlockSymbol? = this[symbol]
+    override fun getReferencedClassifier(symbol: IrClassifierSymbol): IrClassifierSymbol? = this[symbol]
+    override fun getReferencedTypeAlias(symbol: IrTypeAliasSymbol): IrTypeAliasSymbol? = this[symbol]
 }
 
 /**
